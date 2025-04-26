73a9b75a4fae7bc5f4e50fd29d3c496e455f0df9
Bhashkar Yadav <116943212+bhaskar-5625@users.noreply.github.com>
2025-04-26 18:19:04 +0530

    Intitally portfolio commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, I'm Bhashkar</h1>
+      <p>Welcome to my awesome portfolio website.</p>
+      <button onClick={() => alert('Welcome Bhai!')}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
