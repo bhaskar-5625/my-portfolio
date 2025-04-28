a0245626cd662ea686cf5beb768420f96e52d7a9
Bhashkar Yadav <116943212+bhaskar-5625@users.noreply.github.com>
2025-04-28 15:11:00 +0530

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Hello, I'm Bhashkar</h1>
+      <h1>Hello, I'm Ajeet</h1>
       <p>Welcome to my awesome portfolio website.</p>
       <button onClick={() => alert('Welcome Bhai!')}>Click Me</button>
     </div>
