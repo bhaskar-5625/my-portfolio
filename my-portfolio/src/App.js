fc5090ed64ec3b2a9249799ff8d9524b69213d03
Bhashkar Yadav <116943212+bhashkar-yadav@users.noreply.github.com>
2025-05-02 13:40:46 +0530

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,7 +6,7 @@ function App() {
 <<<<<<< HEAD
       <h1>Hi, Hello, I'm Bhashkar Yadav</h1>
 =======
-      <h1>Hello, I'm Bhashkar Yadav</h1>
+      <h1>Hello, I'm Bhashkar Yadav and I am from U.P</h1>
 >>>>>>> a48094d (Updated)
       <p>Welcome to my awesome portfolio website.</p>
       <button onClick={() => alert('Welcome Bhai!')}>Click Me</button>
